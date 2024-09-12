7d04cbb77ddf82cae3eb455f996243792e97afca
Peter <guitaronline89@gmail.com>
2024-09-12 11:32:39 +0300

    initial commit

--- src/main/java/com/dailycodework/lakesidehotel/controller/RoomController.java
+++ src/main/java/com/dailycodework/lakesidehotel/controller/RoomController.java
@@ -1,4 +1,41 @@
 package com.dailycodework.lakesidehotel.controller;
 
+import com.dailycodework.lakesidehotel.model.Room;
+import com.dailycodework.lakesidehotel.response.RoomResponse;
+import com.dailycodework.lakesidehotel.service.RoomService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.SQLException;
+
+@RestController
+@RequestMapping("/api/v1/rooms")
 public class RoomController {
+
+    private final RoomService roomService;
+
+    public RoomController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
+
+    @PostMapping("/add/new-room")
+    public ResponseEntity<RoomResponse> addNewRoom(@RequestParam("photo") MultipartFile photo,
+                                                   @RequestParam("roomType") String roomType,
+                                                   @RequestParam("roomPrice") BigDecimal roomPrice) throws SQLException, IOException {
+
+        Room savedRoom = roomService.addNewRoom(photo, roomType, roomPrice);
+
+        RoomResponse response = new RoomResponse(savedRoom.getId(),
+                savedRoom.getRoomType(),
+                savedRoom.getRoomPrice());
+
+        return ResponseEntity.ok(response);
+    }
 }
--- src/main/java/com/dailycodework/lakesidehotel/service/RoomServiceImpl.java
+++ src/main/java/com/dailycodework/lakesidehotel/service/RoomServiceImpl.java
@@ -1,7 +1,38 @@
 package com.dailycodework.lakesidehotel.service;
 
+import com.dailycodework.lakesidehotel.model.Room;
+import com.dailycodework.lakesidehotel.repository.RoomRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.sql.rowset.serial.SerialBlob;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.sql.SQLException;
 
 @Service
 public class RoomServiceImpl implements RoomService{
+
+    private final RoomRepository repository;
+
+    public RoomServiceImpl(RoomRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Room addNewRoom(MultipartFile photo, String roomType, BigDecimal roomPrice) throws IOException, SQLException {
+
+        Room room = new Room();
+        room.setRoomType(roomType);
+        room.setRoomPrice(roomPrice);
+
+        if(!photo.isEmpty()) {
+
+            byte[] photoByes = photo.getBytes();
+            Blob photoBlob = new SerialBlob(photoByes);
+            room.setPhoto(photoBlob);
+        }
+        return repository.save(room);
+    }
 }
